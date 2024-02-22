4a3e704473ad0a4db188075b2ca01061533a17ef
nyvantran <tondaithanh0@gmail.com>
2024-02-22 21:18:10 +0700

    1

--- quan_ly_may_bay/khai_bao.h
+++ quan_ly_may_bay/khai_bao.h
@@ -1,12 +1,29 @@
 #pragma once
-using namespace std;
-#include <iostream>
-
-void in() {
-	cout << 1;
-}
-
-void test() {
-	cout << "hello meo meo";
-	cout<<1;
-}
+struct Ve
+{
+	char cmnd[225];
+	char vitri1[2];
+	int vitri2;
+};
+enum Trang_Thai {
+	HUY_CHUYEN,
+	CON_VE,
+	HET_VE,
+	HOAN_TAT,
+};
+struct NgayGio
+{
+	int gio;
+	int ngay;
+	int thang;
+	int nam;
+};
+struct ChuyenBay
+{
+	char ma_cb[15];
+	NgayGio ngay_gio_kh;
+	char san_bay_den[40];//hoi thay c[40] c[15]
+	Trang_Thai trang_thai_cb;
+	Ve* ds_ve;
+	ChuyenBay();
+};

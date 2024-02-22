871694eace0c76ff88e5738e913b97e6a75ae8e1
nyvantran <tondaithanh0@gmail.com>
2024-02-22 21:37:45 +0700

    day

--- quan_ly_may_bay/Main.cpp
+++ quan_ly_may_bay/Main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include"khai_bao.h"
 using namespace std;
 int main() {
 }
--- quan_ly_may_bay/khai_bao.h
+++ quan_ly_may_bay/khai_bao.h
@@ -5,25 +5,38 @@ struct Ve
 	char vitri1[2];
 	int vitri2;
 };
-enum Trang_Thai {
+enum TrangThai {
 	HUY_CHUYEN,
 	CON_VE,
 	HET_VE,
 	HOAN_TAT,
 };
-struct NgayGio
-{
-	int gio;
+struct Ngay {
 	int ngay;
 	int thang;
 	int nam;
+};
+
+struct Gio {
+	int gio;
+	int phut;
+	int giay;
+};
+struct NgayGio
+{
+	Ngay ngay_kh;
+	Gio gio_kh;
 };
 struct ChuyenBay
 {
 	char ma_cb[15];
 	NgayGio ngay_gio_kh;
 	char san_bay_den[40];//hoi thay c[40] c[15]
-	Trang_Thai trang_thai_cb;
+	TrangThai trang_thai_cb;
 	Ve* ds_ve;
 	ChuyenBay();
+};
+struct Ds_Chuyen_Bay
+{
+
 };

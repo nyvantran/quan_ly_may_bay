008c75ca14a9abfa0ab0319e7e979397313de023
nyvantran <tondaithanh0@gmail.com>
2024-03-03 21:12:15 +0700

    1

--- quan_ly_may_bay/khai_bao.cpp
+++ quan_ly_may_bay/khai_bao.cpp
@@ -1,1 +1,56 @@
 #include"khai_bao.h"
+//#include<fstream>
+void DSMayBay::napFile(const char file[])
+{
+	fstream file1(file, ios::binary | ios::in);
+	while (!file1.eof()) {
+		maybay[so_MB] = new MayBay;
+		file1.read(reinterpret_cast<char*>(maybay[so_MB++]), sizeof(MayBay));
+	}
+	so_MB--;
+	delete maybay[so_MB];
+	file1.close();
+}
+
+void DSMayBay::ghiFile(const char file[])
+{
+	fstream file1(file, ios::binary | ios::out);
+	for (int i = 0; i < so_MB; i++) {
+		file1.write(reinterpret_cast<char*>(maybay[i]), sizeof(MayBay));
+	}
+	file1.close();
+}
+
+void DSMayBay::xoa()
+{
+	for (int i = 0; i < so_MB; i++) {
+		delete maybay[i];
+	}
+}
+
+void napFileChuyenBay(const char file[], PTRChuyenBay &fist)/*se sua them*/
+{
+	fstream file1(file, ios::binary | ios::in);
+	ChuyenBay doc;
+	PTRChuyenBay here = NULL;
+}
+
+void ghiFileChuyenBay(const char file[], PTRChuyenBay fist)
+{
+	fstream file1(file, ios::binary | ios::out);
+	PTRChuyenBay p;
+	for (p = fist; p->next != NULL; p = p->next) {
+		file1.write(reinterpret_cast<char*>(p->cb.ma_cb), sizeof(char*));
+		file1.write(reinterpret_cast<char*>(&p->cb.ngay_gio_kh), sizeof(NgayGio));
+		file1.write(reinterpret_cast<char*>(p->cb.san_bay_den), sizeof(char*));
+		file1.write(reinterpret_cast<char*>(p->cb.sh_Mb), sizeof(char*));
+		file1.write(reinterpret_cast<char*>(&p->cb.trang_thai_cb), sizeof(TrangThai));
+	}
+}
+
+PTRChuyenBay KhoiTaoCB()
+{
+	PTRChuyenBay p = new NodeChuyenBay;
+	return p;
+}
+
